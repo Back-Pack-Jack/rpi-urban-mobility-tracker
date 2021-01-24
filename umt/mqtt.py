5fb655a0dd631da086c4ac25de28ca81ac6ea781
99Flake <36083052+KingCole01@users.noreply.github.com>
2021-01-24 20:29:13 +0000

    v0.0

---
+++ umt/mqtt.py
@@ -0,0 +1,152 @@
+import os
+import logging
+import signal
+import sys
+import json
+from time import sleep
+import os
+import subprocess
+import time
+import paho.mqtt.client as mqtt 
+import threading
+
+# Initialize Logging
+logging.basicConfig(level=logging.WARNING)  # Global logging configuration
+logger = logging.getLogger("mqtt.MQTT_Client")  # Logger for this module
+logger.setLevel(logging.INFO) # Debugging for this file.
+
+
+    
+# Global Variables
+BROKER_HOST = "192.168.0.13"
+BROKER_PORT = 1883
+CLIENT_ID = "cycle_1"
+TOPIC = [("cycle/live", 2)]
+process = None
+client = None  # MQTT client instance. See init_mqtt()
+logger = logging.getLogger("mqtt.MQTT_Client")
+logger.info("Creating an instance of MQTT_Client")
+'''
+def set_barrier(data):
+
+    livestream = None
+    rfid = None
+    
+    if "rfid" in data:
+        rfid = data["rfid"]
+        
+        if isinstance(rfid, str):
+            rfid_signal(rfid)
+            logger.info("Request for barrier approved")
+        else:
+            logger.info("Request for unknown barrier status of '{}'. We'll null the value.".format(rfid))      
+    
+    elif "livestream" in data:
+        livestream = data["livestream"]
+        
+        if isinstance(livestream, str):
+                Live_Stream.begin(livestream)
+                logger.info("Request for live stream {}.".format(livestream))
+        else:
+            logger.info("Request for unknown live status '{}'. We'll null the value.".format(livestream))
+            livestream = None # off.
+    
+    else:
+        logger.info("Message '{}' did not contain property.".format(data))
+    
+'''
+"""
+MQTT Related Functions and Callbacks
+"""
+def on_connect( client, user_data, flags, connection_result_code):                              
+    """on_connect is called when our program connects to the MQTT Broker.
+    Always subscribe to topics in an on_connect() callback.
+    This way if a connection is lost, the automatic
+    re-connection will also results in the re-subscription occurring."""
+
+    if connection_result_code == 0:                                                            
+        # 0 = successful connection
+        logger.info("Connected to MQTT Broker")
+    else:
+        # connack_string() gives us a user friendly string for a connection code.
+        logger.error("Failed to connect to MQTT Broker: " + mqtt.connack_string(connection_result_code))
+
+    # Subscribe to the topic for LED level changes.
+    client.subscribe(TOPIC)                                                             
+
+
+
+def on_disconnect( client, user_data, disconnection_result_code):                               
+    """Called disconnects from MQTT Broker."""
+    logger.error("Disconnected from MQTT Broker")
+
+
+
+def on_message( client, user_data, msg):                                                         
+    """Callback called when a message is received on a subscribed topic."""
+    logger.debug("Received message for topic {}: {}".format( msg.topic, msg.payload))
+
+    data = None
+
+    try:
+        data = json.loads(msg.payload.decode("UTF-8"))                                         
+    except json.JSONDecodeError as e:
+        logger.error("JSON Decode Error: " + msg.payload.decode("UTF-8"))
+    
+    topics = []
+    for a_topic in TOPIC:
+        topics.append(a_topic[0])
+
+    if msg.topic in topics: 
+        print(topics)                                                                    
+        #set_barrier(data)                                                                    
+        logger.info(data)
+
+    else:
+        logger.error("Unhandled message topic {} with payload ") #+ str(msg.topic, msg.payload)
+
+def on_publish(client, user_data, connection_result_code):
+    pass
+
+def signal_handler( sig, frame):
+    """Capture Control+C and disconnect from Broker."""
+
+    logger.info("You pressed Control + C. Shutting down, please wait...")
+
+    client.disconnect() # Graceful disconnection.
+    sys.exit(0)
+
+
+
+def init_mqtt():
+    global client
+
+    # Our MQTT Client. See PAHO documentation for all configurable options.
+    # "clean_session=True" means we don"t want Broker to retain QoS 1 and 2 messages
+    # for us when we"re offline. You"ll see the "{"session present": 0}" logged when
+    # connected.
+    logger.info("Initialising Client")
+    client = mqtt.Client(
+        client_id=CLIENT_ID,
+        clean_session=False)
+
+    # Route Paho logging to Python logging.
+    client.enable_logger()
+
+    # Setup callbacks
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    
+    # Connect to Broker.
+    client.connect(BROKER_HOST, BROKER_PORT)
+    client.loop_start()
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)  # Capture Control + C
+    logger.info("Listening for messages on topic '" + str(TOPIC) + "'. Press Control + C to exit.")
+
+    init_mqtt()
+    signal.pause()
--- umt/umt_counter.py
+++ umt/umt_counter.py
@@ -4,11 +4,36 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from tkinter import Tk, Canvas, mainloop, PhotoImage, Label
 from PIL import ImageTk, Image 
+import mqtt
+import pickle
+from mqtt import init_mqtt
+
+init_mqtt()
+client = mqtt.client
+ret = client.publish('cycle/count','hello')
+print(ret)
 
 IMG_PATH = 'umt/highway02_frame000010.png'
 CSV_PATH = 'umt/object_paths_highway02_pednet.txt'
 
 gates = []
+detections = []
+
+# load object paths
+df = pd.read_csv(CSV_PATH, header=None, names=['frame', 'time', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'class','score'])
+df.shape
+
+#df.sample(5)
+
+#  compute detection centroids
+df['cx'] = df['bb_left'] + (0.5 * df['bb_width'])
+df['cy'] = df['bb_top']  + (0.5 * df['bb_height'])
+
+my_window = Tk() # Defines Tkinter Window
+background_image = ImageTk.PhotoImage(Image.open(IMG_PATH)) # Creates an img object
+cwidth = background_image.width()
+cheight = background_image.height()
+C = Canvas(my_window,width=cwidth,height=cheight,background='white')
 
 # --- Window for drawing gates --------------------------
 def draw_line(event):
@@ -27,56 +52,54 @@ def draw_line(event):
         C.tag_raise(line)
         click_number=0
 
-my_window = Tk() # Defines Tkinter Window
-background_image = ImageTk.PhotoImage(Image.open(IMG_PATH)) # Creates an img object
 
-width = background_image.width()
-height = background_image.height()
+def create_gates():    
+    C.pack(side='top',fill='both',expand='yes')
+    C.create_image(0,0, image=background_image, anchor='nw')
 
-C = Canvas(my_window,width=width,height=height,background='white')
-C.pack(side='top',fill='both',expand='yes')
-C.create_image(0,0, image=background_image, anchor='nw')
+    C.bind('<Button-1>',draw_line)
 
-C.bind('<Button-1>',draw_line)
-
-click_number=0
-my_window.mainloop()
-print(gates)
+    global click_number
+    click_number=0
+    my_window.mainloop()
 
 
-# load object paths
-df = pd.read_csv(CSV_PATH, header=None, names=['frame', 'time', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'class','score'])
-df.shape
+def display_gates():
+    # load helper image
+    pil_img = Image.open(IMG_PATH)
 
-df.sample(5)
+    # let's validate that these gates are in the right place with a plot
+    fig, ax = plt.subplots(figsize=(15,15))
+    plt.imshow(pil_img)
 
-#  compute detection centroids
-df['cx'] = df['bb_left'] + (0.5 * df['bb_width'])
-df['cy'] = df['bb_top']  + (0.5 * df['bb_height'])
+    print(gates)
 
-# load helper image
-pil_img = Image.open(IMG_PATH)
+    for g, gate in enumerate(gates):
+        x1, y1 = gate[0]
+        x2, y2 = gate[1]
+        x, y = [x1, x2], [y1, y2]
+        plt.plot(x, y, color='orange', linewidth=5)
 
-# let's validate that these gates are in the right place with a plot
-fig, ax = plt.subplots(figsize=(15,15))
-plt.imshow(pil_img)
+        plt.text(
+            sum(x)/2,
+            sum(y)/2 - 10, 
+            f"gate: #{g}", 
+            color='orange', 
+            horizontalalignment='center',
+            fontsize=10)
+            
+    ax.set_aspect('equal')
+    plt.show() # Show plot lines in screen
 
-for g, gate in enumerate(gates):
-    x1, y1 = gate[0]
-    x2, y2 = gate[1]
-    x, y = [x1, x2], [y1, y2]
-    plt.plot(x, y, color='orange', linewidth=5)
-
-    plt.text(
-        sum(x)/2,
-        sum(y)/2 - 10, 
-        f"gate: #{g}", 
-        color='orange', 
-        horizontalalignment='center',
-        fontsize=10)
-        
-ax.set_aspect('equal')
-plt.show() # Show plot lines in screen
+try:
+    with open('boundries.ssg', 'rb') as f:
+        gates = pickle.load(f)
+except FileNotFoundError:
+    create_gates()
+    display_gates()
+    with open("boundries.ssg", "wb") as f:
+        pickle.dump(gates, f)
+    #print('File doesnt exist')
 
 def ccw(a, b, c):
     return (c[1] - a[1]) * (b[0] - a[0]) > (b[1] - a[1]) * (c[0] - a[0])
@@ -86,28 +109,32 @@ def cross(s1, s2):
     c, d = s2
     return ccw(a, c, d) != ccw(b, c, d) and ccw(a, b, c) != ccw(a, b, d) 
 
+
 # now lets cycle throught each objects trajectory and determine if it has crossed either of the gates
-gate_counts_dict = defaultdict(int)
-gate_counts_dict2 = defaultdict(list)
-for n, obj_path in df.groupby(by='id'):
-    
-    # cycle through each time step of trajectory in ascending order
-    for i, row in obj_path.sort_values(by='time', ascending=True).iterrows():
-    
-        # get position at current time
-        xy_t0 = tuple(row[['cx', 'cy']].values)
+def crossed_gates():
+    for n, obj_path in df.groupby(by='id'):
         
-        # get position at most recent historic time step
-        xy_t1 = obj_path[ obj_path['frame'] < row['frame']].sort_values(by='frame', ascending=False)
+        # cycle through each time step of trajectory in ascending order
+        for i, row in obj_path.sort_values(by='time', ascending=True).iterrows():
         
-        # if a previous time step is found, let's check if it crosses any of the gates
-        if xy_t1.shape[0]>0:
-            timecat = list(tuple(xy_t1[['time', 'class']].values[0]))
-            xy_t1 = tuple(xy_t1[['cx', 'cy']].values[0])
+            # get position at current time
+            xy_t0 = tuple(row[['cx', 'cy']].values)
             
-            # cycle through gates
-            for g, gate in enumerate(gates):
-                if cross(gates[g], [xy_t0, xy_t1]):
-                    timecat.insert(0, g)
-                    print(timecat)
+            # get position at most recent historic time step
+            xy_t1 = obj_path[ obj_path['frame'] < row['frame']].sort_values(by='frame', ascending=False)
+            
+            # if a previous time step is found, let's check if it crosses any of the gates
+            if xy_t1.shape[0]>0:
+                timecat = list(tuple(xy_t1[['time', 'class']].values[0]))
+                xy_t1 = tuple(xy_t1[['cx', 'cy']].values[0])
+                
+                # cycle through gates
+                for g, gate in enumerate(gates):
+                    if cross(gates[g], [xy_t0, xy_t1]):
+                        timecat.insert(0, g)
+                        detections.insert(0, timecat)
+                    
+
+crossed_gates()
+print(detections)
 
